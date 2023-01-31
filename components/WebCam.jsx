bf1be9847945bc348c05ff440c019863225d04bb
rubiyet <rubiyetfardous@gmail.com>
2023-01-31 14:15:17 +0600

    webcam component added

---
+++ components/WebCam.jsx
@@ -0,0 +1,40 @@
+import Webcam from "react-webcam";
+import { useRef, useState } from "react";
+import Image from "next/image";
+
+export default function WebCam() {
+  const cameraWidth = 720;
+  const cameraHeight = 720;
+  const aspectRatio = cameraWidth / cameraHeight;
+
+  const videoConstraints = {
+    width: {
+      min: cameraWidth,
+    },
+    height: {
+      min: cameraHeight,
+    },
+    aspectRatio,
+  };
+  const webcamRef = useRef();
+
+  const [image, setImage] = useState();
+
+  function handleCaptureScreenshot() {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImage(imageSrc);
+  }
+  return (
+    <div>{image && <Image src={image} alt={alt} />}
+      {!image && (
+        <Webcam
+          ref={webcamRef}
+          videoConstraints={videoConstraints}
+          width={cameraWidth}
+          height={cameraHeight}
+        />
+      )}
+      <button onClick={handleCaptureScreenshot}>Capture photo</button>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import WebCam from '@/components/WebCam'
 import Head from 'next/head'
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <div>
         <Header />
+        <WebCam />
       </div>
     </>
   )
